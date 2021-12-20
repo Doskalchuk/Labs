28f1dfe1af217fd17481de52adb37905b3fafd3a
Doskalchuk <96442913+Doskalchuk@users.noreply.github.com>
2021-12-21 02:47:15 +0300

    Update part1_rspec.rb

--- laba3/part1_rspec.rb
+++ laba3/part1_rspec.rb
@@ -4,18 +4,18 @@ require './laba3/part1.rb'
 
 RSpec.describe "Main" do
   it "index" do
-    expect{index}.to output("Twilight - fantastic\nAfter - melodrama\nMonster Corporation - cartoon\nShrek - cartoon\n").to_stdout
+    expect{index}.to output("Soul - datactiv\nTitalik - drama\nShadow Hanters - fantastic\nOne pluse one - drama\n").to_stdout
   end
   it "where" do
-    expect(where("melodrama")).to eq(1)
+    expect(where("datactiv")).to eq(1)
  end
  it "update" do
-   expect{index}.to output("Twilight - fantastic\nAfter - melodrama\nMonster Corporation - cartoon\nShrek - cartoon\n").to_stdout
+   expect{index}.to output("Soul - datactiv\nTitalik - drama\nShadow Hanters - fantastic\nOne pluse one - drama\n").to_stdout
  end
  it "find" do
-   expect{find(3)}.to output("Shrek - cartoon\n").to_stdout
+   expect{find(3)}.to output("Shadow Hanters - fantastic\n").to_stdout
  end
  it "delete" do
-   expect{index}.to output("Twilight - fantastic\nAfter - melodrama\nMonster Corporation - cartoon\nShrek - cartoon\n").to_stdout
+   expect{index}.to output("Soul - datactiv\nTitalik - drama\nShadow Hanters - fantastic\nOne pluse one - drama\n").to_stdout
  end
 end
